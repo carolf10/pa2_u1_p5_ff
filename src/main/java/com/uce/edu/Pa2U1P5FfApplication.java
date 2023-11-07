083462ba81d880bcd90d5194011a3d68360f77de
alulestad <vladimirlesta@gmail.com>
2023-11-07 18:58:44 -0500

    Mi primer commit

---
+++ src/main/java/com/uce/edu/Pa2U1P5FfApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P5FfApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5FfApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+	}
+
+}
