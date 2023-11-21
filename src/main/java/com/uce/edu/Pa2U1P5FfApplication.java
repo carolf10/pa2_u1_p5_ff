bf9aa48cc277592ff49e060f96829c5c9c1a19cd
carolf10 <fcarofialloscheca@hotmail.com>
2023-11-20 21:41:45 -0500

    taller 8

--- src/main/java/com/uce/edu/Pa2U1P5FfApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5FfApplication.java
@@ -16,10 +16,13 @@ import com.uce.edu.service.IMateriaService;
 public class Pa2U1P5FfApplication implements CommandLineRunner {
 	
 	@Autowired
-	private IMateriaService iMateriaService;
-	
-	@Autowired
 	private Materia materia;
+
+	@Autowired
+	private Materia materia1;
+
+	@Autowired
+	private Materia materia2;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5FfApplication.class, args);
@@ -27,35 +30,18 @@ public class Pa2U1P5FfApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		this.materia.setCodigo("M1");
-		this.materia.setNombre("P. Avanzada");
-		this.materia.setNumeroCreditos(10);
-		
-		this.iMateriaService.registrar(materia);
-		
-		System.out.println("Reporte inicio");
-		
-		List<Materia> reporte = this.iMateriaService.buscarTodo();
-		for(Materia mat: reporte) {
-			System.out.println(mat);
-		}
-		
-		System.out.println("Reporte fin");
+		this.materia.setNombre("Avanzada II");
+		System.out.println(this.materia);
+		System.out.println(this.materia1);
 
-		
-		Materia mate= this.iMateriaService.buscar("M1");
-		System.out.println(mate);
-		
-		mate.setNumeroCreditos(20);
-		
-		this.iMateriaService.actualizar(mate);
-		Materia mate2= this.iMateriaService.buscar("M1");
-		System.out.println(mate2);
-		
-		this.iMateriaService.eliminar("M1");
-		
-		mate2= this.iMateriaService.buscar("M1");
-		System.out.println(mate2);
+		this.materia1.setNombre("Nuevo nombre");
+		System.out.println(this.materia1);
+		System.out.println(this.materia);
+
+		this.materia2.setNombre("Nombre final");
+		System.out.println(this.materia2);
+		System.out.println(this.materia1);
+		System.out.println(this.materia);
 
 
 	}
